5eeff71245432fa3e6aaa784bbf928febdf569ed
William Fulton <wyfulton@gmail.com>
2016-01-17 15:23:58 -0700

    add final styles

--- public/dist/app.js
+++ public/dist/app.js
@@ -23,12 +23,14 @@ socket.on('connect', function () {
 
 socket.on('message', function (message) {
 	var time = moment.utc(message.timestamp).local().format('h:mma');
-	var $message = jQuery('.messages')
+	var $messages = jQuery('.messages')
+	var $message = jQuery('<li class="list-group-item"></li>')
 
 	console.log('New message:');
 	console.log(message.text);
 	$message.append('<p><strong>' + message.name + ' ' + time + '</strong></p>')
 	$message.append('<p>' + message.text + '</p>')
+	$messages.append($message);
 });
 
 // handles submitting new message
