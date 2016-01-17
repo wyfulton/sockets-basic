b26ba3644a9f948d6555cdeca8fd3fc644836819
William Fulton <wyfulton@gmail.com>
2016-01-17 12:10:10 -0700

    add private chatrooms

--- public/dist/app.js
+++ public/dist/app.js
@@ -5,13 +5,26 @@ var socket = io()
 
 console.log(name + " wants to join " + room);
 
+jQuery('.room-title').text(room)
+
+
+
+
 socket.on('connect', function () {
-	console.log('connected to socket.io server!')
-})
+	console.log('connected to socket.io server!');
+	socket.emit('joinRoom', {
+		name: name,
+		room: room
+	});
+});
+
+
+
 
 socket.on('message', function (message) {
 	var time = moment.utc(message.timestamp).local().format('h:mma');
 	var $message = jQuery('.messages')
+
 	console.log('New message:');
 	console.log(message.text);
 	$message.append('<p><strong>' + message.name + ' ' + time + '</strong></p>')
--- server.js
+++ server.js
@@ -7,14 +7,26 @@ var moment = require('moment');
 
 app.use(express.static(__dirname + '/public'));
 
+var clientInfo = {};
+
 io.on('connection', function (socket) {
 	console.log('user connected via socket.io!');
+
+	socket.on('joinRoom', function (req) {
+		clientInfo[socket.id] = req;
+		socket.join(req.room);
+		socket.broadcast.to(req.room).emit('message', {
+			name: 'System',
+			text: req.name + 'has joined!',
+			timestamp: moment().valueOf()
+		});
+	});
 
 	socket.on('message', function (message) {
 		console.log('Message recieved: ' + message.text);
 		message.timestamp = moment().valueOf();
 
-		io.emit('message', message);
+		io.to(clientInfo[socket.id].room).emit('message', message);
 	});
 
 	socket.emit('message', {
